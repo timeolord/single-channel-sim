fda8ddf55d0a86af3d864a6b267816ce472c65dc
Allan <timeolord6677@gmail.com>
2023-01-26 17:18:40 -0500

    another quick fix

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function drawTrace(data, elementID, timeStep, title, metadata){
   )
 }
 function traceDataTransform(data, index){
-  colors = [
+  const colors = [
     'rgb(255, 99, 132)',
     'rgb(54, 162, 235)',
     'rgb(255, 205, 86)',
